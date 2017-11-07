6734187c9e2a51601a034a9c5fddf53a2d45fb0d
Thiru Thirunavukarasu <thiru0130@gmail.com>
2017-11-06 21:57:54 -0500

    Accuracy calculated

--- src/diffperc/core.clj
+++ src/diffperc/core.clj
@@ -21,7 +21,7 @@
 (defn strip-punctuation
   "TODO: docs"
   [text]
-  (string/replace text #"[^\w\d\s]" ""))
+  (string/replace text #"[^\w\d\s]+" " "))
 
 (defn one-word-per-line
   "TODO: docs"
@@ -42,15 +42,40 @@
     string/lower-case))
 
 (defn diff-files
-  "TODO: docs"
-  [file1 file2]
-  (let [diff (:out (shell/sh "diff" file1 file2))
-        diff-lines (string/split diff #"\n")]
-    (log :debug diff)
-    diff-lines))
+  "Returns a percentage indicating the accuracy of `test-file` against
+  `base-file`.
+  
+  TODO: more docs"
+  [base-file test-file]
+  (let [num-base-file-words (count (string/split (slurp base-file) #"\n"))
+        diff (:out (shell/sh "diff" base-file test-file))
+        diff-lines (string/split diff #"\n")
+        only-in-base-file (re-seq #"(^|\n)< .+" diff)
+        only-in-test-file (re-seq #"(^|\n)> .+" diff)
+        only-in-base-file-count (count only-in-base-file)
+        only-in-test-file-count (count only-in-test-file)
+        accuracy (- 1 (float (/ only-in-base-file-count num-base-file-words)))]
+    (log :debug (str "DIFF LINES: " diff-lines))
+    (log :debug (str "INCORRECT WORDS: " only-in-test-file))
+    (log :info (str "NUM BASE FILE WORDS: " num-base-file-words))
+    (log :info (str "Counted " (count diff-lines) " lines in diff output"))
+    (log :info
+         (str "NUM WORDS ONLY IN BASE (LEFT) FILE: " only-in-base-file-count))
+    (log :info
+         (str "NUM WORDS ONLY IN TEST (RIGHT) FILE: " only-in-test-file-count))
+    (r :success
+       (format "%.0f%%" (* 100 accuracy))
+       {:base-file-words num-base-file-words
+        :missing-words only-in-base-file-count
+        :wrong-words only-in-test-file-count
+        :accuracy accuracy})))
+        
 
 (defn calc-diff-perc
   "Calculate the percentage difference of words between two files.
+
+  Returns a map describing various descrepancy measures between the base and
+  test files.
   
   Punctuation, white-space, and letter casing is ignored.
   
@@ -72,13 +97,18 @@
       :else
       (let [base-file-txt (slurp base-file-path)
             comparable-base-file (make-compare-friendly base-file-txt)
+            base-file-out-path (str base-file-path "--split")
             test-file-txt (slurp test-file-path)
-            comparable-test-file (make-compare-friendly test-file-txt)]
-        (log :debug "BASE FILE:")
-        (log :debug comparable-base-file)
-        (spit (str base-file-path "--split") comparable-base-file)
-        (log :debug "TEST FILE:")
-        (log :debug comparable-test-file)
-        (spit (str test-file-path "--split") comparable-test-file)
-        (diff-files (str base-file-path "--split")
-                    (str test-file-path "--split"))))))
+            comparable-test-file (make-compare-friendly test-file-txt)
+            test-file-out-path (str test-file-path "--split")]
+
+        #_(log :debug (str "BASE FILE:\n" comparable-base-file))
+        (log :info
+             (str "Counted " (count base-file-txt) " characters in BASE file"))
+        (spit base-file-out-path comparable-base-file)
+
+        #_(log :debug (str "TEST FILE:\n" comparable-test-file))
+        (log :info
+             (str "Counted " (count test-file-txt) " characters in TEST file"))
+        (spit test-file-out-path comparable-test-file)
+        (diff-files base-file-out-path test-file-out-path)))))
